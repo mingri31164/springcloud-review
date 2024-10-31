e6993bb29f71cdce91c59fb2b56fc5c241e192bf
mingri <3116430062@qq.com>
2024-11-01 00:54:28 +0800

    feat：es复习

---
+++ hm-common/src/main/java/com/hmall/common/config/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.hmall.common.config;
+
+import com.hmall.common.interceptors.UserInfoInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+//以是否存在springMvc的核心Api来判断是否执行（防止springMvc与网关冲突）
+@ConditionalOnClass(DispatcherServlet.class)
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UserInfoInterceptor());
+    }
+}
---
+++ hm-gateway/src/main/java/com/hmall/gateway/filters/MyGlobalFilter.java
@@ -0,0 +1,8 @@
+package com.hmall.gateway.filters;
+
+
+/**
+ * 全局网关拦截器测试样例
+ */
+public class MyGlobalFilter {
+}
